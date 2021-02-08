9bf5a474cfbf26a2376c9193b8f8b028f776998f
David Liljefors <david.liljefors@futuregames.nu>
2021-02-08 09:38:30 +0100

    project setup finished

---
+++ ogs/src/main.cpp
@@ -0,0 +1,11 @@
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Linking and Project settings done!" << std::endl;
+	return 0;
+}
