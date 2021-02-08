2d4d88d6ab5bf2882b92d4609928b5f2ae905b0d
David Liljefors <david.liljefors@futuregames.nu>
2021-02-08 09:56:35 +0100

    Window creation

--- ogs/src/main.cpp
+++ ogs/src/main.cpp
@@ -4,8 +4,42 @@
 
 #include <iostream>
 
+
+class GLContext
+{
+public:
+	GLContext()
+	{
+		glfwInit();
+		_window = glfwCreateWindow(1280, 720, "ogs", NULL, NULL);
+		
+		if (!_window)
+		{
+			glfwTerminate();
+			std::cerr << "GLFW : Create Window Error" << std::endl;
+			std::exit(-1);
+		}
+	}
+
+	void Run()
+	{
+		while (!glfwWindowShouldClose(_window))
+		{
+			glfwPollEvents();
+
+		}
+	}
+
+private:
+	GLFWwindow* _window = nullptr;
+
+};
+
 int main()
 {
-	std::cout << "Linking and Project settings done!" << std::endl;
+	GLContext ctx;
+	std::cout << "GLFW Window Creating done!" << std::endl;
+	ctx.Run();
+
 	return 0;
 }
