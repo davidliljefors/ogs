361e4f50d6f7c60db387c813c1cfd57be4439818
David Liljefors <david.liljefors@futuregames.nu>
2021-02-10 14:28:39 +0100

    clean up shader creation

--- ogs/src/Shader.cpp
+++ ogs/src/Shader.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <fstream>
 #include <iostream>
+#include <memory>
 
 #include "Shader.h"
 
@@ -16,37 +17,38 @@ Shader::Shader(std::string const& vertex_path, std::string const& fragment_path)
 		return os.str();
 	};
 
-	constexpr auto err_buff_size = 1024;
-	static GLchar shader_error_buffer[err_buff_size];
+	auto compile_shader = [](std::string const& shader_source, int shader_type)
+	{
+		GLchar const* vert_cstr = shader_source.c_str();
+		GLuint const shader = glCreateShader(shader_type);
+		glShaderSource(shader, 1, &vert_cstr, 0);
+		glCompileShader(shader);
 
-	// Vert shader
-	auto const vert_source = read_file(vertex_path);
+		int compile_result;
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &compile_result);
+		if (compile_result == GL_FALSE)
+		{
+			int err_log_len;
+			glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &err_log_len);
 
-	GLchar const* vert_cstr = vert_source.c_str();
-	GLuint const vert_shader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vert_shader, 1, &vert_cstr, 0);
-	glCompileShader(vert_shader);
+			auto const error_log = std::make_unique<char[]>(err_log_len);
+			glGetShaderInfoLog(shader, err_log_len, &err_log_len, error_log.get());
+			std::cout << "Failed to compile " << (shader_type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!\n";
+			std::cerr << error_log.get();
+		}
+		return shader;
+	};
 
-	glGetShaderInfoLog(vert_shader, err_buff_size, NULL, shader_error_buffer);
-	std::cout << "Vertex Shader Compile\n";
-	std::cerr << shader_error_buffer;
-
-	// Frag shader
-	auto const frag_source = read_file(fragment_path);
-
-	GLchar const* frag_cstr = frag_source.c_str();
-	GLuint const frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(frag_shader, 1, &frag_cstr, 0);
-	glCompileShader(frag_shader);
-
-
-	glGetShaderInfoLog(frag_shader, err_buff_size, NULL, shader_error_buffer);
-	std::cout << "Fragment Shader Compile\n";
-	std::cerr << shader_error_buffer;
+	auto const frag_shader = compile_shader(read_file(fragment_path), GL_FRAGMENT_SHADER);
+	auto const vert_shader = compile_shader(read_file(vertex_path), GL_VERTEX_SHADER);
 
 	glAttachShader(program, vert_shader);
 	glAttachShader(program, frag_shader);
 	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(frag_shader);
+	glDeleteShader(vert_shader);
 }
 
 void Shader::Bind() const
